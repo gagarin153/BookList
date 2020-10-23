dc447b0503374b2bc7030db1b99e7195a569a00a
Sultan Aliev <greenstar153@gmail.com>
2020-10-23 19:16:47 +0300

    Added Intresting view

--- BookList/Extensions/UIColorExtensions.swift
+++ BookList/Extensions/UIColorExtensions.swift
@@ -2,5 +2,7 @@ import UIKit
 
 extension UIColor {
     class var softGray: UIColor { UIColor(red: 239/255, green: 239/255, blue: 245/255, alpha: 1) }
+    class var whiteGray: UIColor { UIColor(red: 248/255, green: 241/255, blue: 241/255, alpha: 1) }
+
 }
 
