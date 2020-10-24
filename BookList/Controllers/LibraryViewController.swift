f9dd5ef39421d72da938eabaa543d1bda2a4e6bd
Sultan Aliev <greenstar153@gmail.com>
2020-10-24 13:35:32 +0300

    Fixed constraints

--- BookList/Controllers/LibraryViewController.swift
+++ BookList/Controllers/LibraryViewController.swift
@@ -3,7 +3,9 @@ import UIKit
 class LibraryViewController: UIViewController {
     
     private let scrollView = UIScrollView()
+    
     private let intrestingView = IntrestingView()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +18,7 @@ class LibraryViewController: UIViewController {
     
     private func setUpView() {
         view.addSubview(scrollView)
-        view.addSubview(intrestingView)
+        scrollView.addSubview(intrestingView)
         view.backgroundColor = .softGray
     }
         
@@ -27,18 +29,19 @@ class LibraryViewController: UIViewController {
     
     private func setUpScrollViewConstraint() {
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-        scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        scrollView.contentSize = CGSize(width: view.frame.width, height: view.frame.height + 300)
+
+        scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         
-        scrollView.contentSize = CGSize(width: view.frame.width, height: view.frame.height + 1500)
     }
     
     private func setUpIntrestingViewConstraint() {
-        intrestingView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
-        intrestingView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
-        intrestingView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
+        intrestingView.topAnchor.constraint(equalTo: self.scrollView.topAnchor).isActive = true
+        intrestingView.leadingAnchor.constraint(equalTo: self.scrollView.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
+        intrestingView.trailingAnchor.constraint(equalTo: self.scrollView.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
         intrestingView.heightAnchor.constraint(equalToConstant: 240).isActive = true
     }
 }
