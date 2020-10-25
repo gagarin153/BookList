14bc96e7a3fcf37dad7f512ab7238f123bc81d93
Sultan Aliev <greenstar153@gmail.com>
2020-10-26 00:03:19 +0300

    Changed label name

--- BookList/Views/IntrestingView.swift
+++ BookList/Views/IntrestingView.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class IntrestingView: UIView {
     
-   private let label: UILabel = {
+   private let intrestingLabel: UILabel = {
         let l = UILabel()
         l.translatesAutoresizingMaskIntoConstraints = false
         l.text = "Интересное"
@@ -37,17 +37,17 @@ class IntrestingView: UIView {
     private func setUpSelf() {
         self.backgroundColor = .clear
         self.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(label)
+        self.addSubview(intrestingLabel)
         self.addSubview(containerView)
         self.addSubview(nextButton)
        
     }
     
     private  func setUpConstraints() {
-        label.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+        intrestingLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        intrestingLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
         
-        containerView.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 5).isActive = true
+        containerView.topAnchor.constraint(equalTo: intrestingLabel.bottomAnchor, constant: 5).isActive = true
         containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
